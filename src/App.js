1f5dcb32eb2e17a1ffe123970d99528522d06152
Abdul-Kadir Coskun <Abdulcoskun@hotmail.com>
2022-05-13 20:49:09 +1000

    Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [videoSrc, setVideoSrc] = useState("");
+    const [message, setMessage] = useState("Click Start to transcode");
+    const ffmpeg = createFFmpeg({
+        log: true,
+    });
+    const doTranscode = async () => {
+        setMessage("Loading ffmpeg-core.js");
+        await ffmpeg.load();
+        setMessage("Start transcoding");
+        ffmpeg.FS("writeFile", "test.avi", await fetchFile("./flame.avi"));
+        await ffmpeg.run("-i", "test.avi", "test.mp4");
+        setMessage("Complete transcoding");
+        const data = ffmpeg.FS("readFile", "test.mp4");
+        setVideoSrc(
+            URL.createObjectURL(new Blob([data.buffer], { type: "video/mp4" }))
+        );
+    };
+    return (
+        <div className="App">
+            <p />
+            <video src={videoSrc} controls></video>
+            <br />
+            <button onClick={doTranscode}>Start</button>
+            <p>{message}</p>
+        </div>
+    );
 }
 
 export default App;
