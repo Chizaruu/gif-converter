c15ef48fe45aa1742436fabf2fc0774a4aef91be
Abdul-Kadir Coskun <Abdulcoskun@hotmail.com>
2022-05-14 19:50:46 +1000

    Broke up App.js functions and html return into components
    Signed-off-by: Abdul-Kadir Coskun <Abdulcoskun@hotmail.com>

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Footer() {
+    return (
+        <footer>
+            <p> Made with ❤️ by Abdul-Kadir Coskun</p>
+        </footer>
+    );
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Header() {
+    return (
+        <header>
+            <h1>Gif Converter</h1>
+        </header>
+    );
+}
